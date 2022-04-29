f30a65345e774a26c05793f5548e6580169cbf5b
Ryan Carver <ryan@ryancarver.com>
2022-04-29 10:17:27 -0700

    Add macOS to the Package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "swift-composable-undo",
-  platforms: [.iOS(.v13)],
+  platforms: [.iOS(.v13), .macOS(.v11)],
   products: [
     .library(
       name: "ComposableUndo",
