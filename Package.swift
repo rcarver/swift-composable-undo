21e88a8312ccca58516e4afc7b0791d396f4ea53
Ryan Carver <ryan@ryancarver.com>
2022-09-23 07:11:27 -0700

    update to TCA protocol-beta branch

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.27.0"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "protocol-beta"),
     .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
   ],
   targets: [
